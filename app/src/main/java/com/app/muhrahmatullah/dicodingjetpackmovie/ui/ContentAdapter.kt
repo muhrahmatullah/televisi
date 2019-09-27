31a09479e9ed9da8f671b81dd714684667626dc2
Muh. Rahmatullah <muhammadrahmatullah1@gmail.com>
2019-09-27 21:03:05 +0700

    restructure code

---
+++ app/src/main/java/com/app/muhrahmatullah/dicodingjetpackmovie/ui/ContentAdapter.kt
@@ -0,0 +1,44 @@
+package com.app.muhrahmatullah.dicodingjetpackmovie.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import com.app.muhrahmatullah.dicodingjetpackmovie.R
+import com.app.muhrahmatullah.dicodingjetpackmovie.databinding.ContentItemBinding
+import com.app.muhrahmatullah.dicodingjetpackmovie.entity.Entity
+import com.app.muhrahmatullah.dicodingjetpackmovie.util.AppExecutors
+import com.app.muhrahmatullah.dicodingjetpackmovie.util.DataBoundListAdapter
+
+/**
+ * Created by muh.rahmatullah on 2019-09-26.
+ */
+class ContentAdapter(
+    appExecutors: AppExecutors
+) : DataBoundListAdapter<Entity, ContentItemBinding>(
+    appExecutors = appExecutors,
+    diffCallback = object : DiffUtil.ItemCallback<Entity>() {
+        override fun areItemsTheSame(oldItem: Entity, newItem: Entity): Boolean {
+            return oldItem.title == newItem.title
+        }
+
+        override fun areContentsTheSame(oldItem: Entity, newItem: Entity): Boolean {
+            return oldItem.desc == newItem.desc
+        }
+
+    }
+) {
+    override fun createBinding(parent: ViewGroup): ContentItemBinding {
+        val binding = DataBindingUtil.inflate<ContentItemBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.content_item,
+            parent,
+            false
+        )
+        return binding
+    }
+
+    override fun bind(binding: ContentItemBinding, item: Entity) {
+        binding.movie = item
+    }
+}
---
+++ app/src/main/java/com/app/muhrahmatullah/dicodingjetpackmovie/ui/movies/MoviesViewModel.kt
@@ -0,0 +1,26 @@
+package com.app.muhrahmatullah.dicodingjetpackmovie.ui.movies
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.app.muhrahmatullah.dicodingjetpackmovie.data.FakeRepo
+import com.app.muhrahmatullah.dicodingjetpackmovie.entity.Entity
+import javax.inject.Inject
+
+/**
+ * Created by muh.rahmatullah on 2019-09-24.
+ */
+class MoviesViewModel @Inject constructor(private val fakeRepo: FakeRepo): ViewModel() {
+
+    private val _movieTrigger = MutableLiveData<Boolean>()
+
+    val data : LiveData<List<Entity>> = Transformations.switchMap(_movieTrigger) {
+        fakeRepo.fetchMovie()
+    }
+
+    fun triggerMovie() {
+        _movieTrigger.value = true
+    }
+
+}
