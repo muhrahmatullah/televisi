0b54e1e2ba61988ecdb3472260370eccfed78450
Muh Rahmatullah <muhammadrahmatullah1@gmail.com>
2019-10-19 19:05:02 +0700

    add rest

---
+++ app/src/main/java/com/app/muhrahmatullah/dicodingjetpackmovie/data/MovieRest.kt
@@ -0,0 +1,11 @@
+package com.app.muhrahmatullah.dicodingjetpackmovie.data
+
+import retrofit2.http.GET
+
+/**
+ * Created by muhrahmatullah on 2019-10-12.
+ */
+interface MovieRest {
+
+    @GET
+}
---
+++ app/src/main/java/com/app/muhrahmatullah/dicodingjetpackmovie/data/RemoteDataSource.kt
@@ -0,0 +1,7 @@
+package com.app.muhrahmatullah.dicodingjetpackmovie.data
+
+/**
+ * Created by muhrahmatullah on 2019-10-12.
+ */
+class RemoteDataSource {
+}
