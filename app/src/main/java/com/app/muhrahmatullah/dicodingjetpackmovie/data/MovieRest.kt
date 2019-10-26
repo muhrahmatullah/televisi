fe4ab8bfd4b8ded1f18a2a1a3c124141c9748f23
Muh. Rahmatullah <muhammadrahmatullah1@gmail.com>
2019-10-26 17:30:17 +0700

    add livedata call adapter

--- app/src/main/java/com/app/muhrahmatullah/dicodingjetpackmovie/data/MovieRest.kt
+++ app/src/main/java/com/app/muhrahmatullah/dicodingjetpackmovie/data/MovieRest.kt
@@ -7,5 +7,5 @@ import retrofit2.http.GET
  */
 interface MovieRest {
 
-    @GET
+
 }
---
+++ app/src/main/java/com/app/muhrahmatullah/dicodingjetpackmovie/rest/Status.kt
@@ -0,0 +1,10 @@
+package com.app.muhrahmatullah.dicodingjetpackmovie.rest
+
+/**
+ * Created by muh.rahmatullah on 2019-10-26.
+ */
+enum class Status {
+    SUCCESS,
+    LOADING,
+    ERROR
+}
